2bb8f1b585c5e3f1a36cfe280e1507c3e7e7b289
CompactNet <46925722+CompactNet@users.noreply.github.com>
2019-03-19 16:39:42 +0800

    Add some annotations

--- simulator/evaluator.py
+++ simulator/evaluator.py
@@ -4,6 +4,7 @@ def count_delay(num_layers, num_filters, table_result):
   print(table_result[0][num_filters[0]-1][0],'ms')
   time_delay = table_result[0][num_filters[0]-1][0]
 
+  #accumulate latency of each layer with diff # in/out channels
   for n in range (1, num_layers):
     print(table_result[n][num_filters[n]-1][num_filters[n-1]-1],'ms') 
     time_delay = time_delay + table_result[n][num_filters[n]-1][num_filters[n-1]-1]
@@ -14,6 +15,7 @@ def count_delay(num_layers, num_filters, table_result):
   
 
 if __name__ == '__main__':
+  #MobileNetV2 Model: 18 layers with diff # of filters
   org_num_layers = 18
   org_filters = [32,16,24,24,32,32,32,64,64,64,64,96,96,96,160,160,160,320]
   
@@ -22,6 +24,7 @@ if __name__ == '__main__':
   new_filters = [32, 12, 20, 20, 24, 24, 24, 12, 12, 12, 12, 22, 22, 22, 24, 24, 24, 44]
   #new_filters = [32, 10, 24, 24, 32, 32, 32, 22, 22, 22, 22, 16, 16, 16, 42, 42, 42, 44]
  
+  #open collected data
   f = open('./CPU_224_table_result.pickle', 'rb')
   table_result = pickle.load(f)
   f.close()
@@ -32,6 +35,7 @@ if __name__ == '__main__':
   print("\noptimized_time_per_layer:")
   for new_num_layers in range (1, 19, 1):
     print(new_num_layers)
+    #simulate delay of a new trimmed model
     optimized_time = count_delay(new_num_layers, new_filters, table_result)
 
   accelerated_time = base_time - optimized_time
