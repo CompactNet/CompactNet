b6f1e4c9b0caedcf4e1a6f91dca12ef2a5fc7f0a
CompactNet <46925722+CompactNet@users.noreply.github.com>
2019-04-15 22:22:09 +0800

    Update Liscence

--- simulator/collector.py
+++ simulator/collector.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Weicheng Li, Beihang University. All Rights Reserved.
+# Copyright ***. All Rights Reserved.
 #
 # Licensed under the GNU License, Version 3 (the "License");
 # you may not use this file except in compliance with the License.
